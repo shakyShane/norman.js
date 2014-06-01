7c082e9fd2ea876f2fafa2e397eabb975a480861
Shane Osbourne <shane.osbourne8@gmail.com>
2014-06-01 22:30:16 +0100

    Added map E2E tests

---
+++ test/specs/map.js
@@ -0,0 +1,39 @@
+describe("MAP E2E", function() {
+
+    var mapper;
+
+    beforeEach(function () {
+
+        var viewBox = {
+            height: 300,
+            width: 300,
+            x: 0,
+            y: 0
+        };
+
+        var subject = {
+            height: 400,
+            width: 400
+        };
+
+        mapper = new Norman({viewBox: viewBox}).mapTo(subject);
+    });
+
+    it("Can map values (e2e)", function () {
+        var actual   = mapper.map(150, 150);
+        var expected = {
+            x: -50,
+            y: -50
+        };
+        assert.deepEqual(actual, expected);
+    });
+
+    it("Can map values (e2e) (2)", function () {
+        var actual   = mapper.map(300, 0);
+        var expected = {
+            x: -100,
+            y: 0
+        };
+        assert.deepEqual(actual, expected);
+    });
+});
